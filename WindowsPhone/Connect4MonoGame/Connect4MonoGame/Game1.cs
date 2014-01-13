69f41dde9b2415dc15fac2936960db054f96c1b3
Gustavo Martin <gu.martinm@gmail.com>
2014-01-13 02:26:00 +0100

    Windows Phone: Connect4 MonoGame

---
+++ WindowsPhone/Connect4MonoGame/Connect4MonoGame/Game1.cs
@@ -0,0 +1,94 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Windows;
+
+namespace Connect4MonoGame
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _spriteBatch;
+        Texture2D texture;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            _graphics.SupportedOrientations = DisplayOrientation.LandscapeLeft;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+            
+            
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            if (texture == null)
+            {
+                texture = new Texture2D(_spriteBatch.GraphicsDevice, 1, 1);
+                texture.SetData<Color>(new Color[] { Color.Yellow });
+            }
+            _spriteBatch.Draw(texture, new Rectangle(120, 120, 300, 300), Color.Yellow);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+            
+        }
+    }
+}
---
+++ WindowsPhone/Connect4MonoGame/Connect4MonoGame/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Connect4MonoGame.Resources;
+
+namespace Connect4MonoGame
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
