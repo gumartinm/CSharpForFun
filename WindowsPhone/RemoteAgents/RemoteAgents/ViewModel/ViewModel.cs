109a4db608ba732f041a19e0944ea70e03a77102
Gustavo Martin <gu.martinm@gmail.com>
2014-02-23 21:39:18 +0100

    RemoteAgens .NET Windows Phone implementation

---
+++ WindowsPhone/RemoteAgents/RemoteAgents/ViewModel/ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using RemoteAgents.WindowsPhone.Model;
+using System.Threading.Tasks;
+
+namespace RemoteAgents.WindowsPhone.ViewModel
+{
+    public class ViewModelImpl
+    {
+        private static readonly string uri = "http://gumartinm.name/spring-mainapp/CurrentDateService.json";
+        private readonly CallRemoteProcedure remoteProcedure = new CallRemoteProcedure();
+
+        async public Task<string> getCurrentDate()
+        {
+            return await remoteProcedure.callRemoteService<string>(uri, "getCurrentDate");
+        }
+    }
+}
