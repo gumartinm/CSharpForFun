2d2fb4b366a3e641a0e0dc72f36387c88bdafe25
gu.martinm@gmail.com <gu.martinm@gmail.com>
2014-05-18 03:49:11 +0200

    Threads and C#
    http://parallelpatterns.codeplex.com/
    Chapter 2: Parallel Loops

---
+++ Allgemeines/Threads/Threads/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.Concurrent;
+
+namespace Threads
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Chapter2.Test();
+        }
+    }
+}
