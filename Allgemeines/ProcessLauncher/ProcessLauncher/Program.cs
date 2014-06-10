afd3dd5bf21569f7201198c1e23cfcaa2ccd86c4
Gustavo Martin <gu.martinm@gmail.com>
2014-06-10 06:31:56 +0200

    Trying to change the windows console encoding. I give up.

--- Allgemeines/ProcessLauncher/ProcessLauncher/Program.cs
+++ Allgemeines/ProcessLauncher/ProcessLauncher/Program.cs
@@ -8,11 +8,11 @@ namespace ProcessLauncher
         public static void Main(string[] args)
         {
             LinuxWindowsProcess process = new LinuxWindowsProcess();
-            //Console.WriteLine("Linux");
-            //var linuxOutput = process.Test("ls", "-lah");
-            //Console.WriteLine("stdout: {0}", linuxOutput.ToString());
+            Console.WriteLine("Linux");
+            var linuxOutput = process.Test("ls", "-lah");
+            Console.WriteLine("stdout: {0}", linuxOutput.ToString());
 
-            // I need to change the console font :/ So even using UTF8 encoding this is not going to work. :(
+            // I need to change the console font :/ So, even using UTF8 encoding this is not going to work. :(
             Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("Windows");
             // UTF8 is 65001 code page. :O
